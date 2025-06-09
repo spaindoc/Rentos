26a5dd2e4c879b55fb994903f8c320f460aa359f
Yurpolskyi <fpbidni@gmail.com>
2025-06-09 18:50:15 +0200

    did feedback (with no api)

--- src/app/[locale]/components/feedback.jsx
+++ src/app/[locale]/components/feedback.jsx
@@ -12,46 +12,84 @@ const Feedback = () => {
     const [message, setMessage] = useState('');
 
     const onFetch = async (name, phone, message) => {
-    };//! Напиши будь ласка апішечку, бо я хз які ендпоінти(
+        //! Напиши будь ласка апішечку, бо я хз які ендпоінти(
+    };
 
     return (
-        <section className="bg-auto min-h-[100vh] bg-center bg-no-repeat relative" style={{ backgroundImage: `url(${bg.src})` }}>
-            <div className="border p-2.5 lg:py-2.5 lg:px-5 lg:border-2 border-black uppercase">
-                <h3>{t('Feedback.title')}</h3>
+        <section
+            className="bg-auto pt-[5vh] pb-[10vh] bg-center bg-no-repeat relative overflow-x-hidden min-w-full"
+            style={{ backgroundImage: `url(${bg.src})` }}
+        >
+            <div className={`relative overflow-x-hidden`}>
+                <div className="w-full max-w-full px-4 py-4 box-border">
+
+                    <div className="
+                        w-full max-w-[700px]
+                        mx-auto text-black bg-white
+                        border p-2.5 lg:py-2.5 lg:px-5 lg:border-2 border-black uppercase
+                    ">
+                        <h3 className="break-words leading-tight text-[38px] lg:text-7xl">
+                            {t('Feedback.title')}
+                        </h3>
+                    </div>
+
+                    <form
+                        className="w-full max-w-[700px] mx-auto space-y-4 mt-4 box-border text-black"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            onFetch(name, phone, message);
+                        }}
+                    >
+                        <input
+                            type="text"
+                            placeholder={t('Feedback.form.name')}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className="
+                                w-full max-w-full min-w-0 box-border
+                                border p-2.5 lg:py-2.5 lg:px-5 lg:border-2 border-black
+                                text-base leading-tight
+                                focus:outline-none
+                                bg-white text-[18px] lg:text-[22px] lg:leading-tight
+                            "
+                        />
+
+                        <input
+                            type="tel"
+                            placeholder="+380"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            className="
+                                w-full max-w-full min-w-0 box-border
+                                border p-2.5 lg:py-2.5 lg:px-5 lg:border-2 border-black
+                                text-base leading-tight
+                                focus:outline-none bg-white
+                                text-[18px] lg:text-[22px] lg:leading-tight
+                            "
+                        />
+
+                        <textarea
+                            placeholder={t('Feedback.form.question')}
+                            rows={4}
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            className="
+                                w-full max-w-full min-w-0 box-border
+                                border p-2.5 lg:py-2.5 lg:px-5 lg:border-2 border-black
+                                text-base leading-tight resize-none
+                                focus:outline-none bg-white
+                                text-[18px] lg:text-[22px] lg:leading-tight
+                            "
+                        />
+
+                        <div className="w-full max-w-full box-border">
+                        <button className={`bg-black w-full max-w-[700px] cursor-pointer font-oswald py-4 text-center text-white text-[18px] lg:text-4xl`}>
+                            {t('Feedback.form.submit')}
+                        </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-            <form className="space-y-4 mt-4" onSubmit={(e) => {
-                e.preventDefault();
-                onFetch(name, phone, message);
-            }}>
-                <input
-                    type="text"
-                    placeholder={t('Feedback.form.name')}
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="w-full border p-2.5 lg:py-2.5 lg:px-5 lg:border-2 border-black"
-                />
-
-                <input
-                    type="tel"
-                    placeholder="+380"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    className="w-full border p-2.5 lg:py-2.5 lg:px-5 lg:border-2 border-black"
-                />
-
-                <textarea
-                    placeholder={t('Feedback.form.question')}
-                    rows={4}
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    className="w-full border p-2.5 lg:py-2.5 lg:px-5 lg:border-2 border-black"
-                />
-
-                <Button type="submit">
-                    {t('Feedback.form.submit')}
-                </Button>
-            </form>
         </section>
     );
 };

46452db1999436666876d97896effe50314997e1
Yurpolskyi <fpbidni@gmail.com>
2025-06-12 16:16:45 +0200

    Did partners in normal way

--- src/app/[locale]/components/ui/index.js
+++ src/app/[locale]/components/ui/index.js
@@ -4,5 +4,6 @@ export * from './button';
 export * from './input';
 export * from './textarea';
 export * from './list';
+export * from './infinite-slider';
 export { default as FacebookIcon } from './FacebookIcon';
 export { default as InstagramIcon } from './InstagramIcon'; 

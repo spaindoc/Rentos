0b303a6fd183140589e53766293f23b7c2e8831b
Yurpolskyi <fpbidni@gmail.com>
2025-06-18 15:58:28 +0200

    hot fix #2 (corporate responsibility section)

--- src/components/CorporateResponsibility.jsx
+++ src/components/CorporateResponsibility.jsx
@@ -11,8 +11,9 @@ const CorporateResponsibility = () => {
     return (
         <section className="py-16">
             <Container>
-                    <div className="flex flex-col lg:flex-row items-start lg:block mb-6 md:mb-10 lg:mb-[130px] text-left">
-                        <Heading2 className="mb-6 md:mb-10 lg:mb-[76px] max-w-[300px] lg:max-w-[476px] uppercase leading-10 lg:leading-20 ">
+            <div className="flex flex-col lg:flex-row justify-between mb-6 md:mb-10 lg:mb-[130px]">
+                    <div className="flex flex-col items-left text-center lg:text-left lg:block">
+                        <Heading2 className="mb-6 md:mb-10 lg:mb-[76px] max-w-[476px] uppercase leading-10 lg:leading-20 ">
                             {t('title')}
                         </Heading2>
                         <div className="flex flex-col lg:flex-row items-start gap-4 md:gap-6 lg:gap-8">
@@ -24,23 +25,23 @@ const CorporateResponsibility = () => {
                                 <Image
                                     src={img}
                                     alt="Corporate Responsibility"
-                                    className="w-full  xl:h-full object-cover"
+                                    className="w-full xs:max-w-[300px] sm:max-w-[450px] xs:max-h-[300px] sm:max-h-[450px] lg:h-full object-cover"
                                 />
                             </div>
                             <div className="flex flex-col gap-4 md:gap-6 max-w-[500px] text-gray ">
                                 <Paragraph>{t('sub_2.item_1')}</Paragraph>
                                 <Paragraph>{t('sub_2.item_2')}</Paragraph>
                             </div>
-                            <div className="flex-shrink-0 w-full lg:w-[400px] xl:w-[500px] hidden lg:block">
-                            <Image
-                                src={img}
-                                alt="Corporate Responsibility"
-                                className="w-full h-full object-cover"
-                            />
-                            </div>
                         </div>
                     </div>
-
+                    <div className="flex-shrink-0 w-full lg:w-[400px] xl:w-[500px] hidden lg:block">
+                        <Image
+                            src={img}
+                            alt="Corporate Responsibility"
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+                </div>
 
                 <div className="text-left">
                     <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 mb-6 md:mb-8">
